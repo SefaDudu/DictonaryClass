3171807ccbbe9a63357ef4a05f96f17dbe6ad01c
SefaDudu <dudu.sefaa@gmail.com>
2021-01-21 18:09:47 +0300

    Add project files.

---
+++ DictonaryClass/MyDictonary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryClass
+{
+    class MyDictonary<T1,T2>
+    {
+        //key ve value adında 2 array tanımlayıp dictionary listimizin key ve valuesini alıcağız.
+        T1[] keys;
+        T2[] values;
+        // constructor yapısı ile arraylerimizi Mydictionary her newlendiğinide newleyeceğiz.
+        public MyDictonary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+        //gelen key ve valueyi dictionary yapılarını arraylerimize ekleyeceğiz
+        //bu sayede dictionary yapısının mantığını ele almış olacağız.
+        public void Add(T1 key ,T2 value)
+        {
+            T1[] keyData = keys;
+            T2[] valueData = values;
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+            for (int i = 0; i < keyData.Length; i++)
+            {
+                keys[i] = keyData[i];
+            }
+            for (int i = 0; i < valueData.Length; i++)
+            {
+                values[i] = valueData[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+        public T1[] Keys
+        {
+            get { return keys; }
+        }
+        public T2[] Values
+        {
+            get { return values; }
+        }
+
+
+    }
+}
---
+++ DictonaryClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dictionary yapısı içindeki bir keye bağlı olarak veri tutan bir sözlük olarak düşünebiliriz
+           //Dictionary Çalışma Yapısı
+           /*-----------------------------*/
+            Dictionary<String, int> adYas = new Dictionary<string, int>();
+            adYas.Add("sefa", 21);
+            adYas.Add("ali", 22);
+            adYas.Add("veli", 23);
+            foreach(var degeroku in adYas)
+            {
+                Console.WriteLine(degeroku);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            //Kendi Çalışmamız
+            /*-----------------------------*/
+            MyDictonary<string, int> yasAd2 = new MyDictonary<string, int>();
+            yasAd2.Add("sefa", 24);
+            yasAd2.Add("ali", 25);
+            string[] Key = new string[yasAd2.Length];
+            int[] Value = new int[yasAd2.Length];
+            int x = 0,y=0;
+
+            foreach(var deger in yasAd2.Keys )   { Key[x]   =deger;   x++;}
+            foreach (var deger2 in yasAd2.Values){ Value[y] =deger2;  y++;}
+          
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("["+ Key[i] + "," + Value[i]+"]");
+            }
+        }
+    }
+}
